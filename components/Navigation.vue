064f95a19bd7a4f61b1f4ad7b9de9eda56f4bf63
Sam <sam.white@venris.com>
2021-09-08 19:55:35 +0200

    more setup

---
+++ components/Navigation.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav class="phaltz-navigation">
+    <ul class="phaltz-navigation__navigation-list">
+      <li class="navigation-list__item">
+        Home
+      </li>
+      <li class="navigation-list__item">
+        Home
+      </li>
+      <li class="navigation-list__item">
+        Home
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component<Navigation>({
+  components: {
+    //
+  }
+})
+export default class Navigation extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.phaltz-navigation {
+  @apply w-full;
+  @apply p-4;
+
+  .phaltz-navigation__navigation-list {
+    @apply flex;
+
+    .navigation-list__item {
+      @apply px-1;
+
+      &--test {
+        @apply px-8;
+      }
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,17 @@
 <template>
-<div>
-  <Tutorial/>
-  <Test />
-</div>
+  <div>
+    <Navigation />
+    <Test />
+  </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator';
+import { Vue, Component } from "vue-property-decorator";
 
 @Component<Home>({
-    components: {
-        // 
-    }
+  components: {
+    //
+  }
 })
-export default class Home extends Vue {
-
-}
+export default class Home extends Vue {}
 </script>
