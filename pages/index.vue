218e718b26745853c12922a9ca3b7683e71aef30
Sam <sam.white@venris.com>
2021-09-08 19:32:33 +0200

    nuxt, tailwind, typescript, vuetify installed

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <Tutorial/>
+  <Test />
+</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component<Home>({
+    components: {
+        // 
+    }
+})
+export default class Home extends Vue {
+
+}
+</script>
